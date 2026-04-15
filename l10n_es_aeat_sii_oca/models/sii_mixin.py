2899f30e8b2b0f917fa3394aa051c1e0b78bb47d
Javier Espinoza <jeer2234@gmail.com>
2026-04-15 15:36:39 +0200

    adding temporally fields for upgrade part 4

--- l10n_es_aeat_sii_oca/models/sii_mixin.py
+++ l10n_es_aeat_sii_oca/models/sii_mixin.py
@@ -46,6 +46,21 @@ class SiiMixin(models.AbstractModel):
         copy=False,
         readonly=True,
     )
+    sii_content_sent = fields.Text(
+        string="SII last content sent",
+        copy=False,
+        readonly=True,
+    )
+    sii_state = fields.Selection(
+        selection=SII_STATES,
+        string="SII send state",
+        readonly=True,
+        copy=False,
+        help="Indicates the state of this document in relation with the "
+        "presentation at the SII",
+    ) #delete luego del upgrade
+
+
     ####################################################################
     company_id = fields.Many2one(
         comodel_name="res.company",
@@ -62,15 +77,6 @@ class SiiMixin(models.AbstractModel):
     aeat_state = fields.Selection(
         selection_add=SII_STATES,
     )
-    sii_state = fields.Selection(
-        selection=SII_STATES,
-        string="SII send state",
-        readonly=True,
-        copy=False,
-        help="Indicates the state of this document in relation with the "
-        "presentation at the SII",
-    ) #delete luego del upgrade
-
     sii_csv = fields.Char(string="SII CSV", copy=False, readonly=True)
     sii_return = fields.Text(string="SII Return", copy=False, readonly=True)
     sii_refund_type = fields.Selection(
