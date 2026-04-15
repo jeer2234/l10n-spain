bfa538bd154ac6f45f24c6fc087afb7c3c4430e9
Javier Espinoza <jeer2234@gmail.com>
2026-04-15 13:21:55 +0200

    adding temporally fields for upgrade

--- l10n_es_aeat_sii_oca/models/sii_mixin.py
+++ l10n_es_aeat_sii_oca/models/sii_mixin.py
@@ -139,6 +139,12 @@ class SiiMixin(models.AbstractModel):
         string="Enable SII",
         compute="_compute_sii_enabled",
     )
+    sii_send_failed = fields.Boolean(
+        string="SII send failed",
+        copy=False,
+        help="Indicates that the last attempt to communicate this document to "
+             "the SII has failed. See SII return for details",
+    ) #TODO remove after upgrade
     sii_macrodata = fields.Boolean(
         string="MacroData",
         help="Check to confirm that the document has an absolute amount "
